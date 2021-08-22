a5d558c490eb3ac8b0c29eecb7d9126ea7f9f72e
Andrii Torkotiuk <torkotiuk@gmail.com>
2021-08-22 21:09:53 +0200

    add for swagger ui info about user request

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -4,10 +4,59 @@ const authMiddleware = require('../middlewares/auth');
 
 const { user: ctrl } = require('../controllers');
 
+/**
+ * @swagger
+ * /api/v1/users/signup:
+ *  post:
+ *    description: Use for user registration
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.post('/signup', ctrl.signup);
+
+/**
+ * @swagger
+ * /api/v1/users/login:
+ *  post:
+ *    description: Use to login user
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.post('/login', ctrl.login);
+
+/**
+ * @swagger
+ * /api/v1/users/logout:
+ *  get:
+ *    description: Use to logout user
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.get('/logout', authMiddleware, ctrl.logout);
+
+/**
+ * @swagger
+ * /api/v1/users/current:
+ *  get:
+ *    description: Use to get current user information
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.get('/current', authMiddleware, ctrl.getCurrentUser);
+
+/**
+ * @swagger
+ * /api/v1/users:
+ *  patch:
+ *    description: Use to update user information (name, password, email)
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.patch('/', authMiddleware, ctrl.updateName);
 
 module.exports = router;
