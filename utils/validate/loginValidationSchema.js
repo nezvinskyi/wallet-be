22718916ef805c4ad9649450aa08243647381941
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-08-30 15:26:45 +0300

    Update loginValidationSchema.js

--- utils/validate/loginValidationSchema.js
+++ utils/validate/loginValidationSchema.js
@@ -1,12 +1,12 @@
-const Joi = require('joi')
+const Joi = require('joi');
 
 const loginSchema = newUser => {
   const schema = Joi.object({
     email: Joi.string().email({ minDomainSegments: 2 }).required(),
-    password: Joi.string().min(8).required(),
-  })
-  const { error } = schema.validate(newUser)
-  return error
-}
+    password: Joi.string().min(6).required(),
+  });
+  const { error } = schema.validate(newUser);
+  return error;
+};
 
-module.exports = loginSchema
+module.exports = loginSchema;
