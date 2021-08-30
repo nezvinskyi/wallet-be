b2f5aee7e87363cf4c1bf8c5a6e4645835e690f3
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-08-30 12:07:40 +0300

    Update registrationValidationSchema.js

--- utils/validate/registrationValidationSchema.js
+++ utils/validate/registrationValidationSchema.js
@@ -4,7 +4,7 @@ const validationSchema = newUser => {
   const schema = Joi.object({
     name: Joi.string(),
     email: Joi.string().email({ minDomainSegments: 2 }).required(),
-    password: Joi.string().min(8).required(),
+    password: Joi.string().min(6).required(),
     // confirmPassword: Joi.string().required().valid(Joi.ref('password')),
   });
   const { error } = schema.validate(newUser);
